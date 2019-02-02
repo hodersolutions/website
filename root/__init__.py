7b07729039b260581a6e34537f3f2dcf06c78de9
varanasikalyan <varanasikalyan@icloud.com>
2019-02-02 16:57:21 +0530

    changes
    changes

--- root/__init__.py
+++ root/__init__.py
@@ -9,13 +9,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 application = Flask(__name__, static_url_path='/static')
-application.config.from_pyfile("configs.cfg")
-
-bcrypt = Bcrypt(application)
-login_manager = LoginManager(application)
 
 from controllers import *
