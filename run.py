5df1f44cb8bc160abc1e92ce0f3bf975f5539fad
varanasikalyan <varanasikalyan@icloud.com>
2019-02-02 16:52:05 +0530

    change
    change

--- run.py
+++ run.py
@@ -11,5 +11,4 @@
 #-------------------------------------------------------------------------------
 from root import application
 
-if __name__ == '__main__':
-    application.run(debug=True)
+application.run(debug=False)
